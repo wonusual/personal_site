27f3319dc1e7e1e7012d9cb4e6a0fbac47434e7b
wonusual <80879857+wonusual@users.noreply.github.com>
2021-08-04 21:43:08 +0900

    Add files via upload

--- script/percentage.js
+++ script/percentage.js
@@ -6,18 +6,26 @@ $(function () {
 
     var countUp = new CountUp("statistic1", 0, 70, 0, 2.5, countOptions);
 
-    countUp.start();
+    $(".countup").click(function () {
+        countUp.start();
+    });
 
     var countUp2 = new CountUp("statistic2", 0, 70, 0, 2.5, countOptions);
 
-    countUp2.start();
+    $(".countup").click(function () {
+        countUp2.start();
+    });
 
     var countUp3 = new CountUp("statistic3", 0, 70, 0, 2.5, countOptions);
 
-    countUp3.start();
+    $(".countup").click(function () {
+        countUp3.start();
+    });
 
     var countUp4 = new CountUp("statistic4", 0, 50, 0, 2.5, countOptions);
 
-    countUp4.start();
+    $(".countup").click(function () {
+        countUp4.start();
+    });
 
 });
